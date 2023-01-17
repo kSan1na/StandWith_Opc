ec560347e94ade22931bf267e821ab92b0225ec1
kSan1na <71142650+kSan1na@users.noreply.github.com>
2023-01-17 03:10:00 +0300

    LittleApdate

--- MRTKTest/Assets/Scripts/SingleObjectDisplay.cs
+++ MRTKTest/Assets/Scripts/SingleObjectDisplay.cs
@@ -5,25 +5,58 @@ using UnityEngine;
 public class SingleObjectDisplay : MonoBehaviour
 {
     [SerializeField] private GameObject _stand;
+    [SerializeField] private Material _standText;
     [SerializeField] private GameObject _conveyer1;
+    [SerializeField] private Material _covveyer1Text;
     [SerializeField] private GameObject _conveyer2;
     [SerializeField] private GameObject _buncer;
     [SerializeField] private GameObject _camera;
+    [SerializeField] private GameObject _menu;
     private List<GameObject> _objects;
+    [SerializeField] private GameObject _infopanel;
+    [SerializeField] private GameObject _text;
+    private List<Material> _materials;
+
     int _flag = 0;
     // Update is called once per frame
     private void Start()
     {
-        _objects = new List<GameObject>() { _stand, _buncer, _conveyer1, _conveyer2 };
+        _objects = new List<GameObject>() { _stand, _buncer, _conveyer1, _conveyer2, _menu };
+        _materials = new List<Material>() { _standText, _covveyer1Text };
     }
     private void Replace(int flag)
-    {
+    {   
+        if (flag == 4)
+        {
+            _menu.transform.localScale *= 2;
+        }
+        if (_flag == 4)
+        {
+            _menu.transform.localScale /= 2;
+        }
+        if (flag == 0)
+        {
+            _menu.SetActive(true);
+        }
+        if (_flag == 0)
+        {
+            _menu.SetActive(false);
+        }
+        
         _objects[_flag].SetActive(false);
         _objects[flag].SetActive(true);
-        Vector3 coords =_camera.transform.position;
-        coords.z += 1f;
+        Vector3 coords =_stand.transform.position;
+        coords.y += 0.6f;
+        coords.x += 0.25f;
         _objects[flag].transform.position = coords;
+        coords.x -= 1f;
+        _infopanel.transform.position = coords;
+        Quaternion rotate = _infopanel.transform.rotation;
+        rotate.y = 0;
+        _infopanel.transform.rotation = rotate;
+        _text.GetComponent<MeshRenderer>().material = _materials[1];
         _flag = flag;
+        
     }
     public void PushButton1()
     {
@@ -38,6 +71,11 @@ public class SingleObjectDisplay : MonoBehaviour
     public void PushButton3()
     {
         int flag = 3;
+        Replace(flag);
+    }
+    public void PushMenu()
+    {
+        int flag = 4;
         Replace(flag);
     }
     public void PushHome()
